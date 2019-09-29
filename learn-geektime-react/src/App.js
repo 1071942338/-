af83bc263a404fd0edf446f55e7d756d0c7b05e4
1071942338 <1071942338@qq.com>
2019-09-29 18:25:10 +0800

    添加@material-ui/icons

--- learn-geektime-react/src/App.js
+++ learn-geektime-react/src/App.js
@@ -2,8 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+  },
+});
 function App() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
   return (
     <div className="App">
       <header className="App-header">
@@ -23,6 +36,19 @@ function App() {
       你好，世界
     </Button>
       </header>
+
+      <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
     </div>
   );
 }
