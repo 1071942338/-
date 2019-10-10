727b12aa8e22612c6b7ee02e9dc5e0d9b8544be9
1071942338 <1071942338@qq.com>
2019-10-10 16:21:20 +0800

    添加跨域注解

--- learn-geektime-sever/src/main/java/com/zwq/geekshop/geekshop/BannerController.java
+++ learn-geektime-sever/src/main/java/com/zwq/geekshop/geekshop/BannerController.java
@@ -1,6 +1,7 @@
 package com.zwq.geekshop.geekshop;
 
 import com.alibaba.fastjson.JSON;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+@CrossOrigin
 @RestController
 public class BannerController {
 
